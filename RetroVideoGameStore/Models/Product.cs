f91552fa173f5cf3d2175e57e4cdcd668369286c
MatthewDorioSchool <200618886@student.georgianc.on.ca>
2026-03-05 17:51:03 -0500

    Refind the product index view and the product interface

---
+++ RetroVideoGameStore/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RetroVideoGameStore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:c}")]
+        public double Price { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+        public string? Photo { get; set; }
+        public string? Description { get; set; }
+        // Reference the parent class (1 category -> many products)
+        public Category? Category { get; set; }
+    }
+}
